84892306fbb39f31d72a8988cb56f2a0ac0e090b
Nivetha <nivedha.orton@gmail.com>
2019-08-09 06:42:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function logWhisper(string){
   string = string.toLowerCase();
   console.log(string);
 }
+function sayHiToGrandma(string){
+  if(string === string.toLowerCase()){
+    return "I can\'t hear you!"
+    
+  }
+}
